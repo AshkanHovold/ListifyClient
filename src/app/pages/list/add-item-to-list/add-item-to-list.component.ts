88715efe118d2286b53629e6a5811dd8992f0a74
Ashkan Hovold <ashkan.hovold@gmail.com>
2020-03-15 22:42:29 +0100

    Searching for items to add to list

--- src/app/pages/list/add-item-to-list/add-item-to-list.component.ts
+++ src/app/pages/list/add-item-to-list/add-item-to-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DataService } from 'src/app/shared/data.service';
 import { Constants } from 'src/app/shared/constants';
+import * as _ from "lodash";
 
 @Component({
   selector: 'app-add-item-to-list',
@@ -16,6 +17,9 @@ export class AddItemToListComponent implements OnInit {
   msg: string = "";
   valid: boolean = false;
   availableTemplates: any[];
+  itemsFound: any[] = [];
+  rows: any[];
+  selectedTemplate: string;
   constructor(private route: ActivatedRoute, private dataService: DataService) { }
 
   async ngOnInit() {
@@ -33,8 +37,31 @@ export class AddItemToListComponent implements OnInit {
 
     let all = this.optionSelected === "all";
     if (all) {
-
+      let items = await this.dataService.getAllDataFromStorage(Constants.ITEM);
+      this.itemsFound = this.getSearchResult(items, this.searchTerm);
+      this.rows = _.chunk(this.itemsFound, 3);
+    } else {
+      let template = await this.dataService.getDataFromStorage(Constants.TEMPLATE, this.selectedTemplate);
+      let items = [];
+      for (let i = 0; i < template.items.length; i++) {
+        const item = template.items[i];
+        let toPush = await this.dataService.getDataFromStorage(Constants.ITEM, item.itemId);
+        items.push(toPush);
+        this.itemsFound = this.getSearchResult(items, this.searchTerm);
+        this.rows = _.chunk(this.itemsFound, 3);
+      }
     }
+  }
+  getSearchResult(items: any[], searchTerm: string): any[] {
+    let toReturn = [];
+    items.forEach(i => {
+      let values = i.fields.map(f => (f.data.value));
+      let found = values.filter((v: string) => v.toLowerCase().includes(searchTerm.toLowerCase()));
+      if (found.length > 0) {
+        toReturn.push(i);
+      }
+    })
+    return toReturn;
   }
 
   validate() {
@@ -43,6 +70,9 @@ export class AddItemToListComponent implements OnInit {
       this.valid = false;
     } else if (this.searchTerm.length < 3) {
       this.msg = "Atleast 3 characters required";
+      this.valid = false;
+    } else if (this.optionSelected === 'one' && !this.selectedTemplate) {
+      this.msg = "Select a template or select all templates";
       this.valid = false;
     } else {
       this.msg = "";
